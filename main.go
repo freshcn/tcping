89bf26fba929007b1cd088dd89e3eb146bfad559
chenzhouyu <chenzhouyu@qq.com>
2019-11-22 10:48:25 +0800

    完成tcping主程序

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/urfave/cli"
 	"os"
+	"strings"
+	"tcping/tcping"
+	"time"
 )
 
 func main() {
@@ -11,30 +15,65 @@ func main() {
 
 	app.Name = "tcping"
 	app.EnableBashCompletion = true
+	app.Version = "1.0.0"
+	app.UsageText = "tcping [global options] host"
 	app.Description = "Use the tcp protocol to ping the specified port of the specified website."
 	app.Flags = []cli.Flag{
 		cli.IntFlag{
 			Name:   "port,p",
 			Usage:  "Set the port to request",
-			EnvVar: "port",
 			Hidden: false,
 			Value:  80,
 		},
 		cli.IntFlag{
 			Name:        "timeout,t",
 			Usage:       "Connection timeout",
-			EnvVar:      "timeout",
 			Hidden:      false,
 			Value:       5,
 			Destination: nil,
 		},
 	}
 
-	app.Action = func(c *cli.Context) (err error) {
-		return
-	}
+	app.Action = Run
 
 	if err := app.Run(os.Args); err != nil {
 		fmt.Println(err)
 	}
+}
+
+func Run(c *cli.Context) (err error) {
+	if len(c.Args()) < 1 {
+		return errors.New("parameter cannot be empty")
+	}
+	host := c.Args()[len(c.Args()) - 1]
+
+	if host == "" {
+		return errors.New("parameter cannot be empty")
+	}
+
+	if strings.Index(host, ".") == -1 {
+		return errors.New("wrong parameter")
+	}
+
+	client := tcping.New(tcping.Config{
+		Host:        host,
+		Port:        c.Int("port"),
+		ConnTimeout: time.Duration(c.Int("timeout")) * time.Second,
+	})
+
+	var (
+		i = 0
+	)
+	fmt.Printf("start ping %s:%d\n", host, c.Int("port"))
+	for {
+		data := client.Ping()
+		if data.Err != nil {
+			fmt.Printf("%s\n", data.Err)
+		} else {
+			fmt.Printf("Connected to %s sqe=%d time=%s \n", data.RemoteAddr, i, data.HumanTime())
+		}
+		i++
+		time.Sleep(time.Second * 1)
+	}
+	return
 }
